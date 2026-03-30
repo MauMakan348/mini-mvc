71e338429c36b8b970d9ad83232da2b4c0aec9e0
Putra <vpapaia9@gmail.com>
2026-03-30 09:17:33 +0800

    komit ke 2

--- app/Views/partials/section-accordion.php
+++ app/Views/partials/section-accordion.php
@@ -6,8 +6,14 @@
     <?php foreach ($bantuan as $bantuan_item) : ?>
     <div class="accordion-item">
         <h2 class="accordion-header">
-            <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne"> 
-                <?= $bantuan_item['judul'] ?> </button>
+            <button class="accordion-button" 
+                type="button" 
+                data-bs-toggle="collapse" 
+                data-bs-target="#collapseOne" 
+                aria-expanded="true" 
+                aria-controls="collapseOne"> 
+                <?= $bantuan_item['judul'] ?>
+            </button>
         </h2>
         <div id="collapseOne" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
             <div class="accordion-body">
